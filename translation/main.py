f206b12354280be437f60ddc6f416ae193144db7
Miffle <miffle1337@mail.ru>
2023-07-07 00:15:41 +0300

    reply message fix

--- translation/main.py
+++ translation/main.py
@@ -8,8 +8,7 @@ def translating(message):
     text = message.text
     translated_text = translator.translate(text=text).text
     info = f"Вот перевод: {translated_text}"
-    if len(info) > 4096:
-        for x in range(0, len(info), 4096):
-            bot.send_message(message.chat.id, info[x:x + 4096], reply_markup=startKeyboard)
-    else:
+    if len(translated_text) < 3000:
         bot.send_message(message.chat.id, info, reply_markup=startKeyboard)
+    else:
+        bot.send_message(message.chat.id, "Текст слишком большой, уменьши его", reply_markup=startKeyboard)
