39d36569433d9d216fecad62c8f27ac41d0e02a4
brizzy <iliyagorbunov@yandex.ru>
2023-07-07 14:06:54 +0300

    translator update

--- translation/main.py
+++ translation/main.py
@@ -1,12 +1,15 @@
 from googletrans import Translator
+from langdetect import detect
 import bot_components.reply
 
 
 def translating(message):
     command = "Перевод"
     translator = Translator()
+    src = 'en'
+    dest = 'ru'
     text = message.text
-    translated_text = translator.translate(text=text).text
+    translated_text = translator.translate(message.text, src=src, dest=dest).text
     if len(message.text) < 3000:
         reply_text = f"Вот перевод: {translated_text}"
     else:
